33367da931398ef1fffe41a67be4ecf94ae5db51
Pobitrodeb <pobitrodn@gmail.com>
2023-02-10 12:50:03 +0600

    user create successfully

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -4,8 +4,34 @@ namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\User;
+use Dotenv\Validator;
+
+use Illuminate\Support\Facades\Hash;
+
 
 class UserController extends Controller
 {
-    //
+   public function register(Request $request){
+    $validator = Validator::make($request->all(),[
+            'name'              => 'required|string|min:3|max:25',
+            'email'             => 'required|email|unique:users',
+            'password'          => 'required|password|min:6|confirmed'
+        ]);
+    if($validator->fails()){
+        return response()->json($validator->errors());
+    }
+
+    $user =   User::create([
+        'name'          => $request->name,
+        'email'         => $request->email,
+        'password'      => Hash::make($request->password),
+    ]);
+
+    return response()->json([
+        'message' => 'User Register Create Successfully',
+        'user'    =>$user,
+    ]);
+
+   }
 }
--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\API\UserController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -16,4 +16,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::group(['middleware' => 'api'], function($routes){
+    Route::post('/register', [UserController::class, 'register']);
 });
